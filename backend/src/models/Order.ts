2c7972c23ac2ca08344f856e669c2fec7f8a1d69
Jacqueline Alves <jacque.alves.b@gmail.com>
2021-11-30 21:58:39 -0300

    Updates order model and table to store food id and quantity

--- backend/src/models/Order.ts
+++ backend/src/models/Order.ts
@@ -1,10 +1,12 @@
 export class Order {
     id:number | undefined;
-    merchant_id:number | undefined;
-    client_id:number | undefined;
+    merchant_id:number | undefined; //required
+    client_id:number | undefined; //required
+    food_id:number | undefined; //required
     status:string | undefined;
     total_price:number | undefined;
     payment_method:number | undefined;
     time_ordered:Date | undefined;
     time_delivered:Date | undefined;
+    quantity:number | undefined; //required
 }
