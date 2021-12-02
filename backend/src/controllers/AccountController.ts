1591e8dd5787c93b7c2b9f791cc822d93fc97fb8
Daniel Cavalcanti <dpc2@cin.ufpe.br>
2021-12-01 23:28:48 -0300

    encrypt password

--- backend/src/controllers/AccountController.ts
+++ backend/src/controllers/AccountController.ts
@@ -1,61 +1,96 @@
 import { Request, Response } from "express";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 import { User } from "../models/User";
 import UserRepository from "../repositories/UserRepository";
 
+interface RegisterUserRequest {
+  name: string;
+  email: string;
+  password: string;
+  is_client: boolean;
+  is_merchant: boolean;
+}
+
+interface LoginUserRequest {
+  email: string;
+  password: string;
+}
+
 class AccountController {
+  async register(request: Request, response: Response) {
+    const {
+      name,
+      email,
+      password,
+      is_client,
+      is_merchant,
+    }: RegisterUserRequest = request.body;
 
-    async register(request: Request, response: Response) {
-        const user: User = request.body;
-
-        if (!user.name|| !user.email || !user.password) {
-            return response.status(400).send({ error: "Request was incomplete" });
-        }
-
-        if (user.is_client === undefined && user.is_merchant === undefined) {
-            return response.status(400).send({ error: "Request was incomplete" });
-        }
-
-        const userWithSameEmail = await UserRepository.FindUserByEmail(user.email)
-
-        if (userWithSameEmail) {
-            return response.status(400).send({ error: "Email already registered" });
-        }
-
-        if (await UserRepository.RegisterUser(user)) {
-            return response.status(200).send({ success: "User was registered", user: user });
-        }
-        else {
-            return response.status(500).send({ error: "Something went wrong" });
-        }
+    if (!name || !email || !password) {
+      return response.status(400).send({ error: "Request was incomplete" });
     }
 
-    async login(request: Request, response: Response) {
-        const { email, password } = request.body;
-
-        if (!email|| !password) {
-            return response.status(400).send({ error: 'Request was incomplete' });
-        }
-
-        const user = await UserRepository.FindUserByEmail(email);
-
-        if (!user) return response.status(401).send({ error: 'Invalid login' })
-
-        //the app is just a mvp so security is not that important XD 
-        if (password != user.password) {
-            return response.status(401).send({ error: 'Invalid login' });
-        }
-
-        //TODO: PROCESS ENV
-        const token = jwt.sign({ id: user.id, is_client: user.is_client }, "SEGREDOMUITOSECRETO");
-
-        return response.status(200).send({ auth: true, token: token, user: user });
+    if (is_client === undefined && is_merchant === undefined) {
+      return response.status(400).send({ error: "Request was incomplete" });
     }
 
-    async testLogin(request: Request, response: Response) {
-        return response.status(200).send({id: request.body.user_id, is_client: request.body.is_client});
+    const userWithSameEmail = await UserRepository.FindUserByEmail(email);
+
+    if (userWithSameEmail) {
+      return response.status(400).send({ error: "Email already registered" });
     }
+
+    const password_hash = await bcrypt.hash(password, 8);
+    const createdUser = await UserRepository.RegisterUser({
+      name,
+      email,
+      password_hash,
+      is_client,
+      is_merchant,
+    } as User);
+
+    if (createdUser) {
+      return response
+        .status(200)
+        .send({ success: "User was registered", createdUser });
+    } else {
+      return response.status(500).send({ error: "Something went wrong" });
+    }
+  }
+
+  async login(request: Request, response: Response) {
+    const { email, password }: LoginUserRequest = request.body;
+
+    if (!email || !password) {
+      return response.status(400).send({ error: "Request was incomplete" });
+    }
+
+    const user = await UserRepository.FindUserByEmail(email);
+
+    if (!user) return response.status(401).send({ error: "Invalid login" });
+
+    const match = await bcrypt.compare(password, String(user.password_hash));
+
+    if (!match) {
+      return response.status(401).send({ error: "Invalid login" });
+    }
+
+    //TODO: PROCESS ENV
+    const token = jwt.sign(
+      { id: user.id, is_client: user.is_client },
+      "SEGREDOMUITOSECRETO"
+    );
+
+    return response.status(200).send({ auth: true, token: token, user: user });
+  }
+
+  async testLogin(request: Request, response: Response) {
+    return response
+      .status(200)
+      .send({ id: request.body.user_id, is_client: request.body.is_client });
+  }
 }
 
 export default new AccountController();
--- backend/src/repositories/UserRepository.ts
+++ backend/src/repositories/UserRepository.ts
@@ -3,75 +3,72 @@ import { client } from "../index";
 import { query } from "express";
 
 class UserRepository {
+  async FindAll(): Promise<User[]> {
+    try {
+      const query = await client.query(`SELECT * FROM USERS`);
 
-    async FindAll(): Promise<User[]> {
-        try {
-            const query = await client.query(`SELECT * FROM USERS`);
+      const users = <User[]>query.rows;
 
-            const users = <User[]>query.rows;
-
-            return users;
-        } catch (err) {
-            console.log((err as Error).message)
-            console.log((err as Error).stack)
-        }
-        return []
+      return users;
+    } catch (err) {
+      console.log((err as Error).message);
+      console.log((err as Error).stack);
     }
+    return [];
+  }
 
-    async FindUserById(id: number): Promise<User | undefined> {
+  async FindUserById(id: number): Promise<User | undefined> {
+    try {
+      const query = await client.query(`SELECT * FROM USERS WHERE id = ${id}`);
 
-        try {
-            const query = await client.query(`SELECT * FROM USERS WHERE id = ${id}`);
-
-            if (query.rows.length == 0) {
-                return undefined;
-            }
-            else {
-                return <User>query.rows[0];
-            }
-        } catch (err) {
-            console.log((err as Error).message)
-            console.log((err as Error).stack)
-        }
-
+      if (query.rows.length == 0) {
         return undefined;
+      } else {
+        return <User>query.rows[0];
+      }
+    } catch (err) {
+      console.log((err as Error).message);
+      console.log((err as Error).stack);
     }
 
-    async FindUserByEmail(email: string): Promise<User | undefined> {
-        try {
-            const query = await client.query(`SELECT * FROM USERS WHERE email = '${email}'`);
+    return undefined;
+  }
 
-            if (query.rows.length == 0) {
-                return undefined;
-            }
-            else {                
-                return <User>query.rows[0];
-            }
-        } catch (err) {
-            console.log((err as Error).message)
-            console.log((err as Error).stack)
-        }
+  async FindUserByEmail(email: string): Promise<User | undefined> {
+    try {
+      const query = await client.query(
+        `SELECT * FROM USERS WHERE email = '${email}'`
+      );
+
+      if (query.rows.length == 0) {
         return undefined;
+      } else {
+        return <User>query.rows[0];
+      }
+    } catch (err) {
+      console.log((err as Error).message);
+      console.log((err as Error).stack);
     }
+    return undefined;
+  }
 
-    async RegisterUser(user: User): Promise<boolean> {
-
-        try {
-            if (user.is_client) {
-                await client.query(`INSERT INTO USERS(name,email,password,is_client,is_merchant)\
-                VALUES('${user.name}','${user.email}','${user.password}','t','f')`)
-            }
-            if (user.is_merchant) {
-                await client.query(`INSERT INTO USERS(name,email,password,is_client,is_merchant)\
-                VALUES('${user.name}','${user.email}','${user.password}','f','t')`)
-            }
-            return true;
-        } catch(err) {
-            console.log((err as Error).message)
-            console.log((err as Error).stack)
-        }
-        return false;
+  async RegisterUser(user: User): Promise<boolean> {
+    try {
+      if (user.is_client) {
+        await client.query(`INSERT INTO USERS(name,email,password_hash,is_client,is_merchant)\
+                VALUES('${user.name}','${user.email}','${user.password_hash}','t','f')`);
+      }
+      if (user.is_merchant) {
+        await client.query(`INSERT INTO USERS(name,email,password_hash,is_client,is_merchant)\
+                VALUES('${user.name}','${user.email}','${user.password_hash}','f','t')`);
+      }
+      return true;
+    } catch (err) {
+      console.log((err as Error).message);
+      console.log((err as Error).stack);
     }
+    return false;
+  }
 }
 
-export default new UserRepository()
+export default new UserRepository();
