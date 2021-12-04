b21719d40946a4498e2f43edd32462fc468f34ca
Daniel Cavalcanti <dpc2@cin.ufpe.br>
2021-12-04 02:21:20 -0300

    add and update hooks

--- mobile/hooks/index.tsx
+++ mobile/hooks/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { FoodProvider } from "./food";
+import { OrderProvider } from "./order";
 import { UserProvider } from "./user";
 
 export const AppProvider = ({ children }: any) => {
-  return <UserProvider>{children}</UserProvider>;
+  return (
+    <UserProvider>
+      <FoodProvider>
+        <OrderProvider>{children}</OrderProvider>
+      </FoodProvider>
+    </UserProvider>
+  );
 };
